dafc336ca4c138c9d8c0cbcb30f274acf9ac1357
Kenny <kenny_oliver@icloud.com>
2021-03-28 14:01:14 +0000

    [+] Generate random number, console.log() & document.write() an indexed quote & author from the JSON response

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ function getQuote() {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        console.log(data[0].text);
+        //console.log(data);
+        //console.log(data[0].text);
+        
+        let random_index = Math.floor(Math.random() * 100) + 1;
+        console.log(random_index);
+        
+        console.log(data[random_index].text);
+        console.log(data[random_index].author);
+        
+        document.write(data[random_index].text);
+        document.write(data[random_index].author);
       });
 }
 
